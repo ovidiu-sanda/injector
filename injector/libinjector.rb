12700097b128e1125aea3d968452a99e6d826bbe
Ovidiu <ovidiu.sanda88@gmail.com>
2016-06-23 19:01:12 +0300

    Added InjectorEnum

--- injector/libinjector.rb
+++ injector/libinjector.rb
@@ -1,9 +1,32 @@
 #!/usr/bin/env ruby
 
+class InjectorEnum
+    def initialize v, i=0
+        @v=v
+        @i=i
+    end
+
+    def head
+        @i<@v.size ? @v[@i] : nil
+    end
+
+    def tail
+        InjectorEnum.new @v, @i<@v.size ? @i+1 : @i
+    end
+
+    def any?
+        @i<@v.size
+    end
+end
+
 module LibInjector
     refine Array do
         def tail
             self[1..-1]
+        end
+
+        def to_enum_i
+            InjectorEnum.new self
         end
     end
     refine Hash do
